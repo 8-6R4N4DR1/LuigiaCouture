6d4364b7b2ce62b3b7746fca708736ac4f363f52
GRANADRI <adrianmv25@outlook.es>
2025-11-25 21:11:51 +0100

    env y vendor y autoload y htaccess (fin por hoy)

---
+++ autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+    // Autoloader para cargar las clases de los controladores.
+
+    function controllers_autoload($classname) {
+        $classname = str_replace("\\", "/", $classname);
+        $file = __DIR__ . '/' . $classname . '.php';
+        if (file_exists($file)) require_once $file;
+    }
+
+    spl_autoload_register('controllers_autoload');
+
+?>
